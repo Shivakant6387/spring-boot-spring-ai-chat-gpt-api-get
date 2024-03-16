2739b9d5051dcf0d0ba6084b706a2a442ebcd817
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-16 20:49:58 +0530

    spring boot spring ai chat api

---
+++ src/main/java/com/example/springbootspringai/controller/CustomBotController.java
@@ -0,0 +1,31 @@
+package com.example.springbootspringai.controller;
+import com.example.springbootspringai.dto.ChatGPTRequest;
+import com.example.springbootspringai.dto.ChatGptResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/chatGpt")
+public class CustomBotController {
+
+    @Value("${openai.model}")
+    private String model;
+
+    @Value(("${openai.api.url}"))
+    private String apiURL;
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("prompt") String prompt){
+        ChatGPTRequest request=new ChatGPTRequest(model, prompt);
+        ChatGptResponse chatGptResponse = template.postForObject(apiURL, request, ChatGptResponse.class);
+        return chatGptResponse.getChoices().get(0).getMessage().getContent();
+    }
+}
